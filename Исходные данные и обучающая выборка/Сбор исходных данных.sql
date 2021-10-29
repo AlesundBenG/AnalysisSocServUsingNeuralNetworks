b2e47c49fa36e1ec797135388118d924bbbf9fce
Паша <77670636+AlesundBenG@users.noreply.github.com>
2021-10-29 09:40:54 +0300

    Добавление в скрипт дополнительной извлекаемой информации.

--- Исходные данные и обучающая выборка/Сбор исходных данных.sql
+++ Исходные данные и обучающая выборка/Сбор исходных данных.sql
@@ -1,8 +1,18 @@
---------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------
+
+--Период выборки данных.
+DECLARE @startDateReport  DATE = CONVERT(DATE, '01-01-2019')
+DECLARE @endDateReport    DATE = CONVERT(DATE, '30-09-2021')
+
+
+-------------------------------------------------------------------------------------------------------------------------------
 
 
 --Удаление временных таблиц.
-IF OBJECT_ID('tempdb..#ASSESSMENT_DEPENDENCE') IS NOT NULL BEGIN DROP TABLE #ASSESSMENT_DEPENDENCE END --Оценка зависимостей.
+IF OBJECT_ID('tempdb..#ASSESSMENT_DEPENDENCE')      IS NOT NULL BEGIN DROP TABLE #ASSESSMENT_DEPENDENCE     END --Оценка зависимостей.
+IF OBJECT_ID('tempdb..#SOCIAL_SERVICES')            IS NOT NULL BEGIN DROP TABLE #SOCIAL_SERVICES           END --Назначения на социальное обслуживание.
+IF OBJECT_ID('tempdb..#NUMBER_PROVIDED_SERVICES')   IS NOT NULL BEGIN DROP TABLE #NUMBER_PROVIDED_SERVICES  END --Количество предоставленных услуг.
+IF OBJECT_ID('tempdb..#PERSON_INFO')                IS NOT NULL BEGIN DROP TABLE #PERSON_INFO               END --Информация о получателях социального обслуживания.
 
 
 --------------------------------------------------------------------------------------------------------------------------------
@@ -11,7 +21,6 @@ IF OBJECT_ID('tempdb..#ASSESSMENT_DEPENDENCE') IS NOT NULL BEGIN DROP TABLE #ASS
 --Создание временных таблиц.
 CREATE TABLE #ASSESSMENT_DEPENDENCE (
     PERSONOUID                  INT,    --Идентификатор личного дела.
-    BIRTH_DATE                  DATE,   --Дата рождения.
     INSPECT_FOR_STAZ            INT,    --Оценка зависимости в стационарной форме.
     INSPECT_DATE                DATE,   --Дата проведения обследования.
     INSPECT_INDEX               INT,    --Номер обследования у человека.
@@ -48,20 +57,38 @@ CREATE TABLE #ASSESSMENT_DEPENDENCE (
     SUPPORT_POINT               INT,    --Наличие внешних ресурсов выбранное значение.
     SUPPORT_VALUE               FLOAT   --Наличие внешних ресурсов баллы.
 )
+CREATE TABLE #SOCIAL_SERVICES (
+    SOC_SERV_ID             INT,    --Идентификатор социального обслуживания.
+    SOC_SERV_TYPE           INT,    --Идентификатор типа социального обслуживания.
+    SOC_SERV_START_DATE     DATE,   --Дата начала действия социального обслуживания.
+    SOC_SERV_END_DATE       DATE,   --Дата окончания действия социального обслуживания.
+    ORGANIZATION            INT,    --Идентификатор организации, предоставляющей социальное обслуживание.
+    DEPARTAMENT             INT,    --Идентификатор учреждения, предоставляющего социальное обслуживание.
+    DEGREE                  INT,    --Степень зависимости в посторонней помощи, указанная в социальном обслуживании.
+    PERSONOUID              INT     --Идентификатор личного дела.
+)
+CREATE TABLE #NUMBER_PROVIDED_SERVICES (
+    SOC_SERV_ID     INT,    --Идентификатор социального обслуживания.
+    SERVICE_ID      INT,    --Идентификатор улсуги.
+    SERVICE_COUNT   INT     --Количество предоставленных услуг.
+)
+CREATE TABLE #PERSON_INFO (
+    PERSONOUID  INT,    --Идентификатор личного дела.
+    BIRTH_DATE  DATE    --Дата рождения.
+)
 
 
 ------------------------------------------------------------------------------------------------------------------------------
 
 
 --Выбор оценок зависимости.
-INSERT INTO #ASSESSMENT_DEPENDENCE (PERSONOUID, BIRTH_DATE, INSPECT_FOR_STAZ, INSPECT_DATE, INSPECT_INDEX, INSPECT_NEXT_DATE, ORGANIZATION, DEGREE, SUM_VALUE,
+INSERT INTO #ASSESSMENT_DEPENDENCE (PERSONOUID, INSPECT_FOR_STAZ, INSPECT_DATE, INSPECT_INDEX, INSPECT_NEXT_DATE, ORGANIZATION, DEGREE, SUM_VALUE,
     MOVE_OUTSIDE_HOME_POINT, MOVE_OUTSIDE_HOME_VALUE, APARTMENT_CLEANING_POINT, APARTMENT_CLEANING_VALUE, LAUNDRY_POINT, LAUNDRY_VALUE, COOKING_POINT, COOKING_VALUE,
     MOVING_HOUSE_POINT, MOVING_HOUSE_VALUE, FALLS_POINT, FALLS_VALUE, DRESSING_POINT, DRESSING_VALUE, HYGIENE_POINT, HYGIENE_VALUE, RECEPTION_EAT_MED_POINT, RECEPTION_EAT_MED_VALUE,
     URINATION_DEFECATION_POINT, URINATION_DEFECATION_VALUE, SUPERVISION_POINT, SUPERVISION_VALUE, HEAR_POINT, HEAR_VALUE, PRESENCE_DANGER_POINT, PRESENCE_DANGER_VALUE, SUPPORT_POINT, SUPPORT_VALUE
 )
 SELECT
     PERSONOUID, 
-    BIRTH_DATE, 
     INSPECT_FOR_STAZ, 
     INSPECT_DATE, 
     ROW_NUMBER() OVER (PARTITION BY t.PERSONOUID ORDER BY t.INSPECT_DATE)   AS INSPECT_INDEX,
@@ -99,7 +126,6 @@ SELECT
 FROM (
     SELECT
         personalCard.OUID                                                                           AS PERSONOUID,
-        CONVERT(DATE, personalCard.BIRTHDATE)                                                       AS BIRTH_DATE,
         1                                                                                           AS INSPECT_FOR_STAZ,
         CONVERT(DATE, assesmentStaz.A_DATE_INSPECT)                                                 AS INSPECT_DATE,
         CONVERT(DATE, assesmentStaz.A_DATE_NEXT_INSPECTION)                                         AS INSPECT_NEXT_DATE,
@@ -140,10 +166,10 @@ FROM (
             ON personalCard.OUID = assesmentStaz.PERSONOUID
                 AND personalCard.A_STATUS = 10
     WHERE assesmentStaz.A_STATUS = 10
+        AND CONVERT(DATE, assesmentStaz.A_DATE_INSPECT) BETWEEN @startDateReport AND @endDateReport
     UNION ALL
     SELECT
         personalCard.OUID                                                                           AS PERSONOUID,
-        CONVERT(DATE, personalCard.BIRTHDATE)                                                       AS BIRTH_DATE,
         0                                                                                           AS INSPECT_FOR_STAZ,
         CONVERT(DATE, assesment.A_DATE_INSPECT)                                                     AS INSPECT_DATE,
         CONVERT(DATE, assesment.A_DATE_NEXT_INSPECTION)                                             AS INSPECT_NEXT_DATE,
@@ -182,25 +208,113 @@ FROM (
     ----Личное дело гражданина.
         INNER JOIN WM_PERSONAL_CARD personalCard
             ON personalCard.OUID = assesment.PERSONOUID
-                AND personalCard.A_STATUS = 10
-    WHERE assesment.A_STATUS = 10
+                AND personalCard.A_STATUS = 10 --Статус в БД "Действует".
+    WHERE assesment.A_STATUS = 10 --Статус в БД "Действует".
+        AND CONVERT(DATE, assesment.A_DATE_INSPECT) BETWEEN @startDateReport AND @endDateReport
 ) t
 
 
+------------------------------------------------------------------------------------------------------------------------------
+
+
+--Выбор назначений на социальное обслуживание.
+INSERT INTO #SOCIAL_SERVICES (SOC_SERV_ID, SOC_SERV_TYPE, SOC_SERV_START_DATE, SOC_SERV_END_DATE, ORGANIZATION, DEPARTAMENT, DEGREE, PERSONOUID)
+SELECT DISTINCT
+    socServ.OUID                            AS SOC_SERV_ID,
+    NPD.A_MSP                               AS SOC_SERV_TYPE,
+    CONVERT(DATE, period.STARTDATE)         AS SOC_SERV_START_DATE,
+    CONVERT(DATE, period.A_LASTDATE)        AS SOC_SERV_END_DATE, 
+    socServ.A_ORGNAME                       AS ORGANIZATION,
+    socServ.A_DEPNAME                       AS DEPARTAMENT,
+    socServ.A_DEGREE                        AS DEGREE,
+    socServ.A_PERSONOUID                    AS PERSONOUID
+FROM ESRN_SOC_SERV socServ --Назначение социального обслуживания.
+----Период предоставления МСП.        
+    INNER JOIN SPR_SOCSERV_PERIOD period
+        ON period.A_SERV = socServ.OUID  
+            AND period.A_STATUS = 10 
+            AND (@startDateReport <= CONVERT(DATE, period.A_LASTDATE) OR period.A_LASTDATE IS NULL)
+            AND @endDateReport >= CONVERT(DATE, period.STARTDATE)
+----Нормативно правовой документ.
+    INNER JOIN SPR_NPD_MSP_CAT NPD
+        ON NPD.A_ID = socServ.A_SERV
+----Личное дела, у которых есть оценка зависимости.
+    INNER JOIN #ASSESSMENT_DEPENDENCE assesment
+        ON assesment.PERSONOUID = socServ.A_PERSONOUID 
+WHERE socServ.A_STATUS = 10
+
 
 ------------------------------------------------------------------------------------------------------------------------------
 
 
---Общая выборка.
-SELECT
-    *
+--Выбор количества предоставленных услуг.
+INSERT INTO #NUMBER_PROVIDED_SERVICES (SOC_SERV_ID, SERVICE_ID, SERVICE_COUNT)
+SELECT 
+    AGR.ESRN_SOC_SERV           AS SOC_SERV_ID,
+    tarif.A_SOC_SERV            AS SERVICE_ID,
+    SUM(cosSocServ.A_ACT_QUANT) AS SERVICE_SUM
+FROM WM_COST_SOC_SERV cosSocServ --Стоимость социальных услуг
+----Агрегация по социальной услуге.
+    INNER JOIN WM_SOC_SERV_AGR AGR
+        ON AGR.A_ID = cosSocServ.A_AGR_SOC_SERV
+            AND AGR.A_STATUS = 10
+----Услуги у нужных назначений.
+    INNER JOIN (
+        SELECT DISTINCT SOC_SERV_ID
+        FROM #SOCIAL_SERVICES 
+    ) socServ ON socServ.SOC_SERV_ID = AGR.ESRN_SOC_SERV 
+----Тарифы на социальные услуги.
+    INNER JOIN SPR_TARIF_SOC_SERV tarif
+        ON tarif.A_ID = AGR.A_SOC_SERV
+            AND tarif.A_STATUS = 10
+            AND tarif.A_SOC_SERV IS NOT NULL
+WHERE cosSocServ.A_STATUS = 10
+    AND (@startDateReport <= CONVERT(DATE, cosSocServ.A_DATE_LAST) OR cosSocServ.A_DATE_LAST IS NULL)
+    AND @endDateReport >= CONVERT(DATE, cosSocServ.A_DATE_START)
+    AND ISNULL(cosSocServ.A_ACT_QUANT, 0) > 0
+GROUP BY AGR.ESRN_SOC_SERV, tarif.A_SOC_SERV
+ORDER BY AGR.ESRN_SOC_SERV, tarif.A_SOC_SERV 
+
+
+------------------------------------------------------------------------------------------------------------------------------
+
+
+--Выбор информации о получателях социальных услуг.
+INSERT INTO #PERSON_INFO (PERSONOUID, BIRTH_DATE)
+SELECT DISTINCT
+    personalCard.OUID                       AS PERSONOUID,
+    CONVERT(DATE, personalCard.BIRTHDATE)   AS BIRTH_DATE
+FROM #ASSESSMENT_DEPENDENCE assesment
+    INNER JOIN WM_PERSONAL_CARD personalCard
+        ON personalCard.OUID = assesment.PERSONOUID
+ 
+ 
+------------------------------------------------------------------------------------------------------------------------------
+
+
+--Вывод оценок зависимостей.
+SELECT *
 FROM #ASSESSMENT_DEPENDENCE assesment
 ORDER BY assesment.PERSONOUID, assesment.INSPECT_INDEX
 
+--Вывод назначений.
+SELECT * 
+FROM #SOCIAL_SERVICES socServ
+ORDER BY socServ.PERSONOUID, socServ.SOC_SERV_START_DATE
+
+--Вывод количества оказанных услуг.
+SELECT * 
+FROM #NUMBER_PROVIDED_SERVICES providedServices
+ORDER BY providedServices.SOC_SERV_ID
+
+SELECT * 
+FROM #PERSON_INFO personInfo
+ORDER BY personInfo.PERSONOUID
 
 ------------------------------------------------------------------------------------------------------------------------------
 
 
+/*
 --Сравнение.
 SELECT 
     assesment1.PERSONOUID,
@@ -234,6 +348,5 @@ FROM #ASSESSMENT_DEPENDENCE assesment1
         ON assesment1.PERSONOUID = assesment2.PERSONOUID
             AND assesment1.INSPECT_INDEX = assesment2.INSPECT_INDEX - 1
 ORDER BY assesment1.PERSONOUID, assesment1.INSPECT_INDEX
+*/
 
-
-------------------------------------------------------------------------------------------------------------------------------
